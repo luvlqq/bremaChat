e44b5201f48ae9e608af205999f34720e89c7093
Ivan Lialiuk <lualgod92@gmail.com>
2023-02-20 19:17:48 +0300

    login and signout. JWT and cookie Create a login and signout functions. add a JWT token in auth and cookies.

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AuthService } from './auth/auth.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [AuthModule, PrismaModule, UserModule, JwtModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
