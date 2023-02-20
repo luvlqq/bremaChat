9ce3f7df923ca4be2337c11c0b5bb2d4b531067b
Ivan Lialiuk <lualgod92@gmail.com>
2023-02-20 19:26:52 +0300

    upd and delete useless

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { AuthService } from './auth/auth.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, JwtModule],
+  imports: [AuthModule, PrismaModule, JwtModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
