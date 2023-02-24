ac1bdf0a15b0391656cd46444d7937d515ec4c76
Gleb Katsko <belgokstak@gmail.com>
2023-02-24 23:11:01 +0300

    fixed troubles

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import App from './components/authScreen/App';
 import {QueryClient, QueryClientProvider} from 'react-query'
 
 
